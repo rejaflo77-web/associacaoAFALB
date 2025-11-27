8c1f13b7e005c4ad2706ce693e9e962bd17ef0ab
Danitygo <daniellolita298@gmail.com>
2025-11-27 21:27:45 +0000

    foi removido uma variavel cloudinary

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
 
 require("dotenv").config();
 const express = require("express");
